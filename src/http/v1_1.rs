fe4f503e6d100f29f376bd01608971affa0764cb
batatinha-espacial <antoniobgc10@gmail.com>
2023-06-07 12:34:49 -0300

    Co-authored-by: hrudka.tadeas@outlook.cz <hrudka.tadeas@outlook.cz>

--- src/http/v1_1.rs
+++ src/http/v1_1.rs
@@ -9,11 +9,52 @@ pub struct Request {
 }
 
 impl Request {
-    pub fn parse(data: String) -> Self {
-        
+    pub fn parse(request_str: &str) -> Result<Request, String> {
+        // Split the request into lines
+        let lines: Vec<&str> = request_str.lines().collect();
+
+        // Extract the method and path from the first line
+        let first_line_parts: Vec<&str> = lines[0].split_whitespace().collect();
+        if first_line_parts.len() != 3 {
+            return Err("Invalid request".to_string());
+        }
+        let method = first_line_parts[0].to_string();
+        let path = first_line_parts[1].to_string();
+
+        // Parse the headers and body
+        let mut fields = HashMap::new();
+        let mut body = String::new();
+        let mut parsing_body = false;
+
+        for line in lines.iter().skip(1) {
+            if parsing_body {
+                // Add the line to the body
+                body.push_str(line);
+                body.push_str("\n");
+            } else if line.is_empty() {
+                // Empty line indicates end of headers
+                parsing_body = true;
+            } else {
+                // Parse headers
+                let header_parts: Vec<&str> = line.splitn(2, ": ").collect();
+                if header_parts.len() != 2 {
+                    return Err("Invalid header format".to_string());
+                }
+                let key = header_parts[0].to_string();
+                let value = header_parts[1].to_string();
+                fields.insert(key, value);
+            }
+        }
+
+        Ok(Request {
+            method,
+            path,
+            fields,
+            body,
+        })
     }
 }
-
+// I'm back
 // I'll have to take a break+
 // I'll make the parser but publish the repo first
 #[derive(Debug, Default, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
