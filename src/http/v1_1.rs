7719cb5ee47ec4aeffc7656517e21c5a9fdd6e3f
batatinha-espacial <antoniobgc10@gmail.com>
2023-06-07 15:44:01 -0300

    Co-authored-by: hrudka.tadeas@outlook.cz <hrudka.tadeas@outlook.cz>

--- src/http/v1_1.rs
+++ src/http/v1_1.rs
@@ -1,6 +1,18 @@
 use std::collections::HashMap;
 
-#[derive(Debug, Default, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone)]
+pub enum Error {
+    #[error("Invalid request")]
+    InvalidRequest,
+    #[error("Invalid header format")]
+    InvalidHeaderFormat,
+    #[error("Invalid status")]
+    InvalidStatus,
+}
+
+#[derive(Debug, Default, Clone)]
 pub struct Request {
     pub method: String,
     pub path: String,
@@ -9,14 +21,14 @@ pub struct Request {
 }
 
 impl Request {
-    pub fn parse(request_str: &str) -> Request {
+    pub fn parse(request_line: String) -> Result<Request, Error> {
         // Split the request into lines
-        let lines: Vec<&str> = request_str.lines().collect();
+        let lines: Vec<String> = request_line.lines().map(|s| s.to_string()).collect();
 
         // Extract the method and path from the first line
-        let first_line_parts: Vec<&str> = lines[0].split_whitespace().collect();
+        let first_line_parts: Vec<String> = lines[0].split_whitespace().map(|s| s.to_string()).collect();
         if first_line_parts.len() != 3 {
-            return Err("Invalid request".to_string());
+            return Err(Error::InvalidRequest);
         }
         let method = first_line_parts[0].to_string();
         let path = first_line_parts[1].to_string();
@@ -30,15 +42,15 @@ impl Request {
             if parsing_body {
                 // Add the line to the body
                 body.push_str(line);
-                body.push_str("\n");
+                body.push('\n');
             } else if line.is_empty() {
                 // Empty line indicates end of headers
                 parsing_body = true;
             } else {
                 // Parse headers
-                let header_parts: Vec<&str> = line.splitn(2, ": ").collect();
+                let header_parts: Vec<String> = line.split(':').map(|s| s.to_string()).collect();
                 if header_parts.len() != 2 {
-                    return Err("Invalid header format".to_string());
+                    return Err(Error::InvalidHeaderFormat);
                 }
                 let key = header_parts[0].to_string();
                 let value = header_parts[1].to_string();
@@ -46,26 +58,27 @@ impl Request {
             }
         }
 
-        Request {
+        Ok(Request {
             method,
             path,
             fields,
             body,
-        }
+        })
     }
-    pub fn to_string(self) -> String {
+}
+
+impl core::fmt::Display for Request {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut result = String::from(format!("{} {} HTTP/1.1\r\n", self.method, self.path));
         for i in self.fields.keys() {
             result += format!("{}: {}\r\n", i, self.fields.get(i).unwrap()).as_str();
         }
         result += format!("\r\n{}", self.body).as_str();
-        result
+        write!(f, "{}", result)
     }
 }
-// I'm back
-// I'll have to take a break+
-// I'll make the parser but publish the repo first
-#[derive(Debug, Default, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+
+#[derive(Debug, Default, Clone)]
 pub struct Response {
     pub status: u16,
     pub fields: HashMap<String, String>,
@@ -73,17 +86,20 @@ pub struct Response {
 }
 
 impl Response {
-    pub fn parse(data: String) -> Self {
+    pub fn parse(data: String) -> Result<Self, Error> { // No need to parse the response
         // Split the request into lines
-        let lines: Vec<&str> = request_str.lines().collect();
+        let lines: Vec<&str> = data.lines().collect();
 
         // Extract the method and path from the first line
         let first_line_parts: Vec<&str> = lines[0].split_whitespace().collect();
         if first_line_parts.len() != 2 && first_line_parts.len() != 3  {
-            return Err("Invalid request".to_string());
+            return Err(Error::InvalidRequest);
         }
 
-        let status = first_line_parts[1].to_string() as u16;
+        let status: u16 = match first_line_parts[1].to_string().as_str().parse() {
+            Ok(v) => v,
+            Err(_e) => return Err(Error::InvalidStatus)
+        };
 
         let mut fields = HashMap::new();
         let mut body = String::new();
@@ -99,9 +115,9 @@ impl Response {
                 parsing_body = true;
             } else {
                 // Parse headers
-                let header_parts: Vec<&str> = line.splitn(2, ": ").collect();
+                let header_parts: Vec<String> = line.splitn(2, ": ").map(|s| s.to_string()).collect();
                 if header_parts.len() != 2 {
-                    return Err("Invalid header format".to_string());
+                    return Err(Error::InvalidHeaderFormat);
                 }
                 let key = header_parts[0].to_string();
                 let value = header_parts[1].to_string();
@@ -109,15 +125,54 @@ impl Response {
             }
         }
 
-        Self { status: status, fields: fields, body: body }
+        Ok(Self { status: status, fields: fields, body: body })
     }
-    pub fn to_string(self) -> String {
-        let mut result = String::from(format!("HTTP/1.1 {} \r\n"));
+}
+
+impl core::fmt::Display for Response {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut result = String::from(format!(
+            "HTTP/1.1 {}\r\n{:#?}\r\n\r\n{}",
+            self.status,
+            self.fields.iter().map(|(k, v)| format!("{k}:{v}")).collect::<Vec<String>>(),
+            self.body,
+        ));
         for i in self.fields.keys() {
             result += format!("{}: {}\r\n", i, self.fields.get(i).unwrap()).as_str()
         }
         result += format!("\r\n{}", self.body).as_str();
-        result
+        write!(f, "{}", result)
     }
 }
+
+// same as tcp, but handle takes a Request and returns a Response
+
+use std::net::{TcpListener, TcpStream};
+use std::io::{Result as IoResult, Read, Write};
+use core::fmt::Display;
+
+pub fn server<D: Display, F: Fn(Request) -> Response>(host: D, port: u16, handle: F) -> IoResult<()> {
+    let listener = TcpListener::bind(format!("{}:{}", host, port))?;
+
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+        let mut data = String::new();
+        stream.read_to_string(&mut data)?;
+        let request = Request::parse(data).unwrap();
+        let response = handle(request);
+        stream.write_all(response.to_string().as_bytes())?;
+        stream.flush()?;
+    }
+
+    Ok(())
+}
+
+pub fn client<D: Display>(host: D, port: u16, request: Request) -> IoResult<Response> {
+    let mut stream = TcpStream::connect(format!("{}:{}", host, port))?;
+    stream.write_all(request.to_string().as_bytes())?;
+    let mut data = String::new();
+    stream.read_to_string(&mut data)?;
+    Ok(Response::parse(data).unwrap())
+}
+
 // https://httpwg.org/specs/rfc9112.html
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,10 +1,11 @@
 use std::net::{TcpStream, TcpListener};
 use std::io::Result as IoResult;
+use core::fmt::Display;
 
 pub fn server<D: Display, F: Fn(&mut TcpStream) -> IoResult<()>>(host: D, port: u16, handle: F) -> IoResult<()> {
     let listener = TcpListener::bind(format!("{}:{}", host, port))?;
 
-    for mut stream in listener.incoming() {
+    for stream in listener.incoming() {
         let mut stream = stream?;
 
         handle(&mut stream)?;
